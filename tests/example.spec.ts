342c57d730157bea9135f7e9a5f03a1352dea34e
Güney Kaya <guney.kaya@portalgrup.com>
2025-02-10 16:12:54 +0300

    test 1

---
+++ tests/example.spec.ts
@@ -0,0 +1,141 @@
+import { test, expect, Page } from '@playwright/test';
+import * as nodemailer from 'nodemailer';
+
+
+const websites = [
+  'https://fenerium.com/',
+  'https://www.fenerbahce.org/',
+  'https://www.dynavit.com.tr/',
+  'https://www.sunpettr.com.tr/',
+  'https://eczaneden.com/',
+  'https://otobil.opet.com.tr/',
+  'https://www.selpak.com.tr/',
+  'https://www.avis.com.tr/',
+  'https://www.budget.com.tr/',
+  'https://www.otokoc.com.tr/',
+  'https://www.koc.com.tr/',
+  'https://www.kocailem.com/',
+  "https://www.florence.com.tr/",
+  "https://www.yapikrediyarinlarakartopu.com.tr/",
+  "https://www.avisfilomaestro.com.tr/",
+  "https://www.avisfilo.com/",
+  "https://www.amerikanhastanesi.org/",
+  "https://www.amerikanhastanesi.org/bodrum-amerikan-hastanesi",
+  "https://www.amerikanhastanesi.org/amerikan-tip-merkezi",
+  "https://www.kuh.ku.edu.tr/",
+  "https://www.yapikrediportfoy.com.tr/",
+  "https://www.tofasanadoluarabalarimuzesi.com",
+  "https://www.opar.com/",
+  "https://www.tofas.com.tr/",
+  "https://www.alfaromeo.com.tr/",
+  "https://www.otomobil.fiat.com.tr/",
+  "https://www.jeep.com.tr/",
+  "https://www.kocfiatkredi.com.tr/",
+  "https://online.kocfiatkredi.com.tr/",
+  "https://www.otokar.com.tr/",
+  "https://commercial.otokar.com.tr/",
+  "https://defense.otokar.com.tr/",
+  "https://www.otokarcentralasia.kz/en",
+  "https://www.tokenflex.com.tr/",
+  "https://www.tokeninc.com/",
+  "https://oderopay.com.tr/",
+  "https://www.turktraktor.com.tr/",
+  "https://caseih.com.tr/",
+  "https://www.caseismakineleri.com/",
+  "https://www.newholland.com.tr/",
+  "https://www.newhollandismakineleri.com/",
+  "https://kiralama.tofas.com.tr/",
+  "https://randevu.jeep.com.tr/",
+  "https://randevu.fiat.com.tr/",
+  "https://randevu.alfaromeo.com.tr/",
+  "https://www.sanipakprofessional.com.tr/",
+  "https://www.sanipak.com.tr/",
+  "https://egos.com.tr/",
+  "https://defans.com.tr/",
+  "https://detan.com.tr/",
+  "https://selin.com.tr/",
+  "https://www.hijyendemukemmellik.com/",
+  "https://www.budget.hu/en", 
+  "https://www.solo.com.tr/"
+
+
+
+];
+
+
+async function sendEmail(screenshotPath: string, websiteUrl: string) {
+  
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: 'portalgrup.test.otomasyon@gmail.com', 
+      pass: 'isye jwlg zokt waow', 
+    },
+  });
+
+  
+  const recipients = [
+    'otokocmobiltest@portalgrup.com',
+    
+    
+  ];
+
+ 
+  const screenshotName = `-${new URL(websiteUrl).hostname}.png`;
+
+  
+  const mailOptions = {
+    from: 'portalgrup.test.otomasyon@gmail.com',
+    to: recipients.join('otokocmobiltest@portalgrup.com'), 
+    subject: `Siteye Ulaşılamadı: ${websiteUrl}`,
+    text: `Siteye ulaşılamamaktadır: ${websiteUrl}. Ekran görüntüsü ektedir. -PG Test Ekibi, Site Kontrol Test Otomasyonu \n\n`,
+    attachments: [
+      {
+        filename: screenshotName, 
+        path: screenshotPath,
+      },
+    ],
+  };
+
+  
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`E-posta gönderildi: ${websiteUrl}`);
+  } catch (error) {
+    console.error(`E-posta gönderilemedi: ${error.message}`);
+  }
+}
+
+
+websites.forEach((url) => {
+  test(`Check status code and take screenshot for ${url}`, async ({ page }) => {
+    let response;
+    try {
+      
+      response = await page.goto(url, { waitUntil: 'networkidle' });   //daha sonra domcontentloaded kullanarak da test et
+
+      
+      if (response && !response.ok()) {
+        console.log(`Çökük Site: ${url} - Status Code: ${response.status()}`);
+
+       
+        const screenshotPath = `screenshots/${new URL(url).hostname}.png`;
+        await page.screenshot({ path: screenshotPath });
+
+       
+        await sendEmail(screenshotPath, url);
+      } else {
+        console.log(`Site Çalışıyor: ${url} - Status Code: ${response?.status()}`);
+      }
+    } catch (error) {
+      console.error(`Siteye erişilemiyor: ${url} - Hata: ${error.message}`);
+
+    
+      const screenshotPath = `screenshots/${new URL(url).hostname}.png`;
+      await page.screenshot({ path: screenshotPath });
+
+      
+      await sendEmail(screenshotPath, url);
+    }
+  });
+});
